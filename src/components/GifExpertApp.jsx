714dd6c3a92df1f52f793f8c361095b55e333de3
Daniel Ojeda <leinadpy@gmail.com>
2021-09-02 04:50:37 -0400

    first commit

---
+++ src/components/GifExpertApp.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useState } from "react";
+import AddCategory from "./AddCategory";
+import GifGrid from "./GifGrid";
+
+const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch"]);
+
+  return (
+    <>
+      <h2>GifExpertApp</h2>
+      <AddCategory setCategories={setCategories} />
+      <hr />
+      <ol>
+        {categories.map((category) => (
+          <GifGrid key={category} category={category} />
+        ))}
+      </ol>
+    </>
+  );
+};
+
+export default GifExpertApp;
